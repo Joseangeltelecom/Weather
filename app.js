16e1f3d8e7b25441b304de739cfdae0e8890882d
Jose Angel <joseangeltelecom@gmail.com>
2021-07-04 09:15:10 -0500

    .

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const container = document.querySelector(".content")
 const input = document.querySelector(".input")
 const form = document.querySelector(".form")
 
-form.addEventListener("submit", async function getData(e,city) {
-
-  const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=28fe7b5f9a78838c639143fc517e4343`;
+form.addEventListener("submit", async function getData(e, city) {
+  const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=28fe7b5f9a78838c639143fc517e4343`
   try {
-    e.preventDefault();
-    const response = await fetch(endpoint, { mode: "cors" });
-    if (!response.ok) throw new Error(`City ${city} not found`);
-    const data = await response.json();
+    e.preventDefault()
+    const response = await fetch(endpoint, { mode: "cors" })
+    if (!response.ok) throw new Error(`City ${city} not found`)
+    const data = await response.json()
     console.log(data)
     container.innerHTML = ` 
     <div class="cards" >
@@ -21,9 +20,9 @@ form.addEventListener("submit", async function getData(e,city) {
         <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
     </div>
 `
-    return data;
+    return data
   } catch (error) {
     alert(error)
-    return null;
+    return null
   }
 })
